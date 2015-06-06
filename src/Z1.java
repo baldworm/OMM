525772f40ad0f5ca946463b3ea7faf1a384b3cd1
bwbw <n.bald.worm@gmail.com>
2015-06-06 19:27:35 +0300

    ez

--- src/Z1.java
+++ src/Z1.java
@@ -2,9 +2,6 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
-/**
- * Created by bw on 25.05.2015.
- */
 public class Z1 {
     private static final int N_X = 80;
     private static final int N_T = 80;
@@ -12,16 +9,14 @@ public class Z1 {
 
     private static double[][] u = new double[N_T][N_X];
 
-    private static final double DX = -1./N_X;
-    private static final double DT = 1./N_T;
+    private static final double DX = -1. / N_X;
+    private static final double DT = 1. / N_T;
 
     public static void main(String[] args) throws IOException {
         boundConditions();
         solver();
         writeToFile("dataZ1.txt");
     }
-
-
 
     private static void boundConditions() {
         for (int i = 0; i < N_X; i++)
@@ -30,49 +25,56 @@ public class Z1 {
         for (int i = 0; i < N_T; i++)
             u[i][0] = 0;
     }
+
     private static void solver() {
-        for (int i = 0; i < N_T - 1; i++){
-            for (int j = 0; j < N_X - 1; j++){
-                u[i+1][j+1] = solve(i,j);
+        for (int i = 0; i < N_T - 1; i++) {
+            for (int j = 0; j < N_X - 1; j++) {
+                u[i + 1][j + 1] = solve(i, j);
             }
         }
         System.out.println("Solved");
     }
+
     private static double solve(int i, int j) {
         double u_old = 1, u_new = 0;
-        while (Math.abs(u_new - u_old)>EPS){
+        while (Math.abs(u_new - u_old) > EPS) {
             u_old = u_new;
-            u_new = u_old - equation(u_old, i, j)/d_equation(u_old);
+            u_new = u_old - equation(u_old, i, j) / d_equation(u_old);
         }
         return u_new;
     }
+
     private static double equation(double x, int i, int j) {
-        return (x - u[i][j+1])/DT + (f(x) - f (u[i+1][j]))/DX;
+        return (x - u[i][j + 1]) / DT + (f(x) - f(u[i + 1][j])) / DX;
     }
+
     private static double d_equation(double x) {
-        return 1/DT + df(x)/DX;
+        return 1 / DT + df(x) / DX;
     }
-    private static double f(double x){
-        return -Math.log(1 + Math.exp(2*x));
+
+    private static double f(double x) {
+        return -Math.log(1 + Math.exp(2 * x));
     }
-    private static double df(double x){
-        return -2*Math.exp(2*x)/(1 + Math.exp(2*x));
+
+    private static double df(double x) {
+        return -2 * Math.exp(2 * x) / (1 + Math.exp(2 * x));
     }
 
     private static void writeToFile(String fileName) throws IOException {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))){
-            for (int i = 0;i < N_T; i++){
-                for (int j = 0; j < N_X; j++){
-                    bufferedWriter.write(getX(i)+"\t"+getT(j)+"\t"+u[i][j]+"\n");
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            for (int i = 0; i < N_T; i++) {
+                for (int j = 0; j < N_X; j++) {
+                    bufferedWriter.write(getX(i) + "\t" + getT(j) + "\t" + u[i][j] + "\n");
                 }
             }
         }
     }
 
-    private static double getX(int i){
+    private static double getX(int i) {
         return i * DX;
     }
-    private static double getT(int i){
+
+    private static double getT(int i) {
         return i * DT;
     }
 
